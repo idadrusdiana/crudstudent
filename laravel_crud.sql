e08138b05cbb1fa8b6c19346e706661ef935b50d
Idad Rusdiana <idadrusdiana01@gmail.com>
2020-07-07 14:56:54 +0700

    database

--- laravel_crud.sql
+++ laravel_crud.sql
@@ -1,13 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 4.6.5.2
+-- version 5.0.2
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 22, 2020 at 07:52 AM
--- Server version: 10.1.21-MariaDB
--- PHP Version: 7.1.2
+-- Generation Time: Jul 07, 2020 at 09:54 AM
+-- Server version: 10.4.13-MariaDB
+-- PHP Version: 7.4.7
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -31,7 +32,7 @@ CREATE TABLE `guru` (
   `nama` varchar(100) NOT NULL,
   `telepon` varchar(15) NOT NULL,
   `alamat` text NOT NULL,
-  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
   `updated_at` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -55,7 +56,7 @@ CREATE TABLE `mapel` (
   `nama` varchar(191) NOT NULL,
   `semester` varchar(45) NOT NULL,
   `guru_id` int(11) NOT NULL,
-  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
   `updated_at` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -79,7 +80,7 @@ CREATE TABLE `mapel_siswa` (
   `siswa_id` int(11) NOT NULL,
   `mapel_id` int(11) NOT NULL,
   `nilai` int(11) NOT NULL,
-  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
   `updated_at` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -145,6 +146,30 @@ CREATE TABLE `password_resets` (
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `posts`
+--
+
+CREATE TABLE `posts` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `content` text NOT NULL,
+  `slug` varchar(255) NOT NULL,
+  `thumbnail` varchar(255) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `posts`
+--
+
+INSERT INTO `posts` (`id`, `user_id`, `title`, `content`, `slug`, `thumbnail`, `created_at`, `updated_at`) VALUES
+(1, 11, 'Ini berita pertama', '<p>Ini isi dari berita pertama yang di input secara manual </p>', 'ini-berita-pertama', '', '2020-07-07 04:22:18', '0000-00-00 00:00:00');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `siswa`
 --
 
@@ -166,19 +191,18 @@ CREATE TABLE `siswa` (
 --
 
 INSERT INTO `siswa` (`id`, `user_id`, `nama_depan`, `nama_belakang`, `jenis_kelamin`, `agama`, `alamat`, `avatar`, `created_at`, `updated_at`) VALUES
-(4, 0, 'Muhammad', 'Gagan', 'L', 'Islam', 'Depok', NULL, '2020-04-21 17:00:00', '2020-04-24 09:14:42'),
+(4, 0, 'Muhammad', 'Gagan', 'L', 'Islam', 'Jakarta', NULL, '2020-04-21 17:00:00', '2020-06-25 07:59:55'),
 (5, 0, 'Iman', 'Supriadi', 'L', 'Islam', 'Bekasi', NULL, '2020-04-21 17:00:00', '2020-04-24 09:13:48'),
-(6, 0, 'Andi', 'Sulaeman', 'L', 'Islam', 'Bandung', NULL, '2020-04-23 10:21:01', '2020-04-23 10:21:01'),
 (7, 0, 'Didin', 'Afifuddin', 'L', 'Islam', 'Majalengka', NULL, '2020-04-23 10:27:37', '2020-04-23 10:27:37'),
-(9, 0, 'Muhammad', 'Taufiq', 'L', 'Islam', 'Cirebon', NULL, '2020-04-24 07:53:57', '2020-04-24 07:53:57'),
-(10, 0, 'Ikhwan', 'Maulana', 'L', 'Islam', 'Cirebon', NULL, '2020-04-24 09:15:16', '2020-04-24 09:15:16'),
 (11, 0, 'Anwar', 'Nurrahman', 'L', 'Islam', 'Bandung', NULL, '2020-04-24 10:25:16', '2020-04-24 10:25:16'),
-(12, 0, 'Andi', 'Kurniawa', 'L', 'Islam', 'Ciledug', NULL, '2020-04-27 09:22:47', '2020-04-27 09:22:47'),
-(13, 3, 'Azil', 'Ilham', 'L', 'Islam', 'Bandung', NULL, '2020-04-27 09:53:52', '2020-04-27 09:53:52'),
-(15, 6, 'Mahmud', 'Azhari', 'L', 'Islam', 'Jakarta', NULL, '2020-04-29 11:33:41', '2020-04-29 11:33:41'),
-(16, 7, 'Muhammad', 'Reynaldi', 'L', 'Islam', 'Bekasi', NULL, '2020-04-29 11:38:40', '2020-04-29 11:38:40'),
-(17, 8, 'Rifaldi', 'Akhbar', 'L', 'Islam', 'Bandung', 'D:\\xampp\\tmp\\phpC6C.tmp', '2020-04-29 11:51:38', '2020-04-29 11:51:38'),
-(18, 9, 'Muhammad', 'Yusuf', 'L', 'Islam', 'Bandung', 'fron NPIC.jpeg', '2020-04-29 11:55:05', '2020-04-29 11:55:05');
+(20, 13, 'idad', 'Rusdiana', 'L', 'Islam', 'Bandung Barat', 'Logo-politeknik-tedc-bandung.png', '2020-06-26 09:15:47', '2020-06-26 09:18:13'),
+(21, 14, 'Faiz', 'Muhammad', 'L', 'Islam', 'Purwakarta', NULL, '2020-06-30 01:35:41', '2020-06-30 01:35:41'),
+(22, 15, 'Ujang', 'Sarip', 'L', 'Islam', 'Sukabumi', NULL, '2020-06-30 01:38:53', '2020-06-30 01:38:53'),
+(24, 19, 'Ikhwan', 'Maulana', 'L', 'Islam', 'Cirebon', NULL, '2020-07-01 21:50:31', '2020-07-01 21:50:31'),
+(26, 21, 'Muhammad', 'Gilang', 'L', 'Islam', 'Cirebon', NULL, '2020-07-01 22:07:40', '2020-07-01 22:07:40'),
+(27, 22, 'Tufiq', 'M', 'L', 'Islam', 'Cianjur', NULL, '2020-07-02 01:17:49', '2020-07-02 01:17:49'),
+(28, 23, 'Ovie', 'Sri', 'P', 'Islam', 'Yogyakarta', NULL, '2020-07-02 07:30:32', '2020-07-02 07:30:32'),
+(29, 24, 'Zaki', 'Rayhan', 'L', 'Islam', 'Surabaya', NULL, '2020-07-02 09:32:34', '2020-07-02 09:32:34');
 
 -- --------------------------------------------------------
 
@@ -210,7 +234,17 @@ INSERT INTO `users` (`id`, `role`, `name`, `email`, `email_verified_at`, `passwo
 (8, 'siswa', 'Rifaldi', 'akhbar@gmail.com', NULL, '$2y$10$1iA1yRY4DprFAZaHQUhqieOPt62hixP7MvR6j3HuzW3lKq2lsmdKa', 'aVlPF3lELGYKCnUOighMHXQsxNwH4i5LxLYwekXcfmwyrcnBNFkQEEvnKB7L', '2020-04-29 11:51:38', '2020-04-29 11:51:38'),
 (9, 'siswa', 'Muhammad', 'muhammadyusuf@gmail.com', NULL, '$2y$10$ne.q/8VICB3Mr7.f6BKmiel2uFjoPeM0seI2i3ZHyeE.cnntxSB0y', 'mg4bkXK2Wt5foy21MJFSGsRnGeufs1u726NjSemD07ops5TZjtHWZWfCCmBr', '2020-04-29 11:55:05', '2020-04-29 11:55:05'),
 (10, 'admin', 'idad', 'rolloic@gmail.com', NULL, '$2y$10$WG9Fq8ghDypmmYn7sfFp9erMrKnHCV4ldxIGfNX8ULlvE0ba7OZ6i', 'Hvg16UdsmXBuZ1PLV5VbjIqZGKVnkNpfiTTY2mdDry7ZWAxV4wLBQSA1goYY', '2020-05-26 04:20:13', '2020-05-26 04:20:13'),
-(11, 'admin', 'Anwar', 'anwar@gmail.com', NULL, '$2y$10$xThMzPNl.9oS3q9bSNK.BOCjOz6Zziwy6MxPJe4053I7uVHNo3qye', 'FZKu23wdyEOyhzWLolsoT8ZbnFiSw4XSoz93j1e0hl4bdws8MOURQwquTsdi', '2020-05-26 14:21:22', '2020-05-26 14:21:22');
+(11, 'admin', 'Anwar', 'anwar@gmail.com', NULL, '$2y$10$xThMzPNl.9oS3q9bSNK.BOCjOz6Zziwy6MxPJe4053I7uVHNo3qye', 'y1YDS282sEAWZPYqCqWhfqTVlt0HpcyN4pw0n10zC2z68MWeQt884Skd4n9S', '2020-05-26 14:21:22', '2020-05-26 14:21:22'),
+(12, 'siswa', 'Muhammad', 'topik@gmail.com', NULL, '$2y$10$sVFjSYNVi1YyZ4pm3Nd3xOLctVkFirLKqQ4ASqRW39gN9Ltx1inny', 'zXW7SSQc5fR7ukAYKBrRx4MQ2ThrjTRrlHNMLu7SM4oUT7owmOg1JAQObt6o', '2020-06-25 10:24:07', '2020-06-25 10:24:07'),
+(13, 'siswa', 'idad', 'idadrusdiana01@gmail.com', NULL, '$2y$10$gE4UxzA8O6ZPNR/bOsS8teHgIp05JOdDimrZPfQOY01qzYnocx1LK', 'h9cewPxXH31fnWqGPjebaEwvAqyFnYrj9wzpIXG37cbeBCbM4biBw31t6rGv', '2020-06-26 09:15:47', '2020-06-26 09:15:47'),
+(14, 'siswa', 'Faiz', 'faiz@gmail.com', NULL, '$2y$10$LrCLNO/K2E5BOXsYydG5XusavmMgslFFj5UN0zYtEgQ6sbeUT1Ai.', 'JsgFJlHkFgC9NZwMscm3TJfYnBXzeRNZfLGTyVZ7B1dqhBqqWpu2z22RyFhr', '2020-06-30 01:35:41', '2020-06-30 01:35:41'),
+(15, 'siswa', 'Ujang', 'ujang@gmail.com', NULL, '$2y$10$3pSYb2EAs5M9pLvAMLOMseIpZVbsbIdSo27tYmo8nZIcHAcQoPzum', 'GtHsK1g611EsJeOHvltTUi0OyzWpfP1nLXD4p3BLaRP6rEOLWXOtt7x3nd8m', '2020-06-30 01:38:53', '2020-06-30 01:38:53'),
+(16, 'siswa', 'Muhammad', 'rivan@gmail.com', NULL, '$2y$10$uTqv4.qChRez6qhk9INe4ewab1nM4C4WMb33NSvLnK0UJl2GahT4m', 'jjIlJEoCHIv0e2yNK9baA7x2bbuk2cvmEsvZ36BuOcAXLhoVHeVURfs6psX0', '2020-07-01 21:44:17', '2020-07-01 21:44:17'),
+(19, 'siswa', 'Ikhwan', 'ikhwan@gmail.com', NULL, '$2y$10$xE5b/V3w6xxvM89/VJOtOOsjq4waTfSyUiX6slHXITZJEGzdeY5bi', 'nZIU5tcIxtxpVgyBatZSnFjNAQ09M8h5QlhBzMcyZ7HIB2q4DSsfDC6wNtxm', '2020-07-01 21:50:30', '2020-07-01 21:50:30'),
+(21, 'siswa', 'Muhammad', 'gilang@gmail.com', NULL, '$2y$10$B0AK7SAtTDkOFUMAeCP1L.4IsJR6mNSBe9hEYzNmI.mkD4aPM90.K', 'vxP0eh2gVUML81BLEFfDt1LdlstxUqU3SdL8TxqazxjgvBJCLBg3pfVPvQGC', '2020-07-01 22:07:40', '2020-07-01 22:07:40'),
+(22, 'siswa', 'Tufiq', 'taufiq@gmail.com', NULL, '$2y$10$1UehrfifS/EuJshSt7ycreuulNqcY48/prgGhhmTB6Rh9JF0vgiw6', 'g3doWKUItmuwYaXUamm9hHYnx30APUBv0mnlrqYaxLDOC3Zz8WxYlfOHUzqp', '2020-07-02 01:17:48', '2020-07-02 01:17:48'),
+(23, 'siswa', 'Ovie', 'ovie@gmail.com', NULL, '$2y$10$9H5Qg.jrALEL1yCwzW5wauROhGZYBni06JeyGNRLmtgU3f2GFxTJm', '8xjQ0WrREQ1KJY0Ol5w0qP7byQVv9Jan7Nv3OmN1F4FbTHqQyY5dQqozhRa2', '2020-07-02 07:30:32', '2020-07-02 07:30:32'),
+(24, 'siswa', 'Zaki', 'zaki@gmail.com', NULL, '$2y$10$rMzk7szgq1AgHb4PN/ZgPugGrLqJr1UbNKR2nYIN01mP5s/oQLvcS', 'fgej9WH9fyoNXnXMeRdkXEaENumwuoR3Fh9R6CYJqm6lyoRgyV94eyuHXNeh', '2020-07-02 09:32:34', '2020-07-02 09:32:34');
 
 --
 -- Indexes for dumped tables
@@ -247,6 +281,12 @@ ALTER TABLE `password_resets`
   ADD KEY `password_resets_email_index` (`email`);
 
 --
+-- Indexes for table `posts`
+--
+ALTER TABLE `posts`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `siswa`
 --
 ALTER TABLE `siswa`
@@ -268,31 +308,44 @@ ALTER TABLE `users`
 --
 ALTER TABLE `guru`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
 --
 -- AUTO_INCREMENT for table `mapel`
 --
 ALTER TABLE `mapel`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
 --
 -- AUTO_INCREMENT for table `mapel_siswa`
 --
 ALTER TABLE `mapel_siswa`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
+
 --
 -- AUTO_INCREMENT for table `migrations`
 --
 ALTER TABLE `migrations`
   MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `posts`
+--
+ALTER TABLE `posts`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
 --
 -- AUTO_INCREMENT for table `siswa`
 --
 ALTER TABLE `siswa`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
+
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
+COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
